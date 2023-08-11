3f142e9c4a280eb68046902fe894b7fb0642657e
Peter Reutemann <fracpete@gmail.com>
2023-08-11 16:22:45 +1200

    initial commit

---
+++ DESCRIPTION.rst
@@ -0,0 +1,3 @@
+The **llm-dataset-converter** allows the conversion between
+various dataset formats for large language models (LLMs).
+
---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup
+
+
+def _read(f):
+    """
+    Reads in the content of the file.
+    :param f: the file to read
+    :type f: str
+    :return: the content
+    :rtype: str
+    """
+    return open(f, 'rb').read()
+
+
+setup(
+    name="llm-dataset-converter",
+    description="Python3 library for converting between various LLM dataset formats.",
+    long_description=(
+        _read('DESCRIPTION.rst') + b'\n' +
+        _read('CHANGES.rst')).decode('utf-8'),
+    url="https://github.com/waikato-datamining/llm-dataset-converter",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Programming Language :: Python :: 3',
+    ],
+    license='MIT License',
+    package_dir={
+        '': 'src'
+    },
+    packages=[
+        "ldc",
+    ],
+    install_requires=[
+        "pandas",
+        "jsonlines",
+        "pyarrow",
+        "datasets",
+    ],
+    version="0.0.1",
+    author='Peter Reutemann',
+    author_email='fracpete@waikato.ac.nz',
+)
