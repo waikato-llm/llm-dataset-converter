5917bf213d400afdf6693382f863ed5352ac3a58
Peter Reutemann <fracpete@gmail.com>
2023-09-05 16:18:37 +1200

    added missing MetaData filter

--- src/ldc/filter/__init__.py
+++ src/ldc/filter/__init__.py
@@ -1,5 +1,6 @@
 from ._core import Filter, MultiFilter, FILTER_ACTIONS, FILTER_ACTION_KEEP, FILTER_ACTION_DISCARD
 from ._keyword import Keyword
+from ._metadata import MetaData
 from ._pairs_to_pretrain import PairsToPretrain
 from ._skip_duplicate_ids import SkipDuplicateIDs
 from ._skip_duplicate_text import SkipDuplicateText
