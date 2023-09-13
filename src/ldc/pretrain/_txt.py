ec26a3ab88c45b1e23461e001b8aecf18a32daf7
Peter Reutemann <fracpete@gmail.com>
2023-09-14 09:10:06 +1200

    in concatenation mode only logging output filename once now

--- src/ldc/pretrain/_txt.py
+++ src/ldc/pretrain/_txt.py
@@ -370,8 +370,9 @@ class TxtPretrainWriter(StreamPretrainWriter):
         """
         if self._concatenate:
             mode = "w" if self._first_item else "a"
+            if self._first_item:
+                self.logger().info("Writing to: %s" % self.target)
             self._first_item = False
-            self.logger().info("Writing to: %s" % self.target)
             with open(self.target, mode) as fp:
                 fp.write(data.content)
                 fp.write("\n")
