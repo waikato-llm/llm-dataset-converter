099ad9c6b1d7a49e17ba143c9a954c587d150209
Peter Reutemann <fracpete@gmail.com>
2023-10-18 13:26:00 +1300

    str_to_column_index now handles None as column input

--- src/ldc/utils.py
+++ src/ldc/utils.py
@@ -3,13 +3,18 @@ def str_to_column_index(c: str, fail_on_nondigit=False):
     Attempts to turn the string column (name or 1-based column index)
     into a 0-based integer index.
 
-    :param c: the column name/index to process
+    :param c: the column name/index to process, can be None
     :type c: str
     :param fail_on_nondigit: raises an exception if not a numeric index
     :type fail_on_nondigit: bool
     :return: the index or -1 if not an index
     :rtype: int
     """
+    if c is None:
+        if fail_on_nondigit:
+            raise Exception("Failed to parse column: %s" % c)
+        else:
+            return -1
     if c.isdigit():
         try:
             return int(c) - 1
