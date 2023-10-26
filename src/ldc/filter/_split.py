9c37627e8eb74a727e33b53433dd19b781252c75
Peter Reutemann <fracpete@gmail.com>
2023-10-26 13:35:14 +1300

    moved splitting code into ldc.utils.Splitter class

--- src/ldc/filter/_split.py
+++ src/ldc/filter/_split.py
@@ -1,6 +1,4 @@
 import argparse
-import numpy as np
-
 from typing import List
 
 from ldc.core import DOMAIN_ANY, LOGGING_WARN, get_metadata, MetaDataHandler
@@ -8,7 +6,7 @@ from ldc.filter import Filter
 from ldc.pretrain import PretrainData
 from ldc.supervised.pairs import PairData
 from ldc.translation import TranslationData
-
+from ldc.utils import Splitter
 
 META_SPLIT = "split"
 """ the key for storing the split name in the meta-data. """
@@ -36,9 +34,7 @@ class Split(Filter):
         super().__init__(logger_name=logger_name, logging_level=logging_level)
         self.split_ratios = split_ratios
         self.split_names = split_names
-        self._schedule = None
-        self._counter = 0
-        self._stats = None
+        self._splitter = None
 
     def name(self) -> str:
         """
@@ -113,29 +109,17 @@ class Split(Filter):
         Initializes the processing, e.g., for opening files or databases.
         """
         super().initialize()
-        if self.split_ratios is None:
-            raise Exception("No split ratios defined!")
-        if self.split_names is None:
-            raise Exception("No split name defined!")
-        if len(self.split_ratios) != len(self.split_names):
-            raise Exception("Differing number of split ratios and names: %d != %d" % (len(self.split_ratios), len(self.split_names)))
-        if sum(self.split_ratios) != 100:
-            raise Exception("Split ratios must sum up to 100, but got: %d" % sum(self.split_ratios))
 
-        # compute greatest common denominator and generate schedule
-        gcd = np.gcd.reduce(np.asarray(self.split_ratios))
-        self._schedule = [0] * (len(self.split_ratios) + 1)
-        for i, ratio in enumerate(self.split_ratios):
-            self._schedule[i + 1] = self._schedule[i] + ratio / gcd
-        self._counter = 0
-        self._stats = dict()
+        self._splitter = Splitter(self.split_ratios, self.split_names)
+        self._splitter.initialize()
 
     def _output_stats(self):
         """
         Outputs the statistics.
         """
-        for split in self._stats:
-            self.logger().info("%s: %d" % (split, self._stats[split]))
+        stats = self._splitter.stats()
+        for split in stats:
+            self.logger().info("%s: %d" % (split, stats[split]))
 
     def _do_process(self, data):
         """
@@ -145,10 +129,10 @@ class Split(Filter):
         :return: the potentially updated record or None if to drop
         """
         if self._has_input_changed(update=True):
-            if self._counter > 0:
-                self.logger().info("Input changed, resetting counter.")
+            if self._splitter.counter() > 0:
+                self.logger().info("Input changed, resetting splitter.")
                 self._output_stats()
-                self._counter = 0
+                self._splitter.reset()
 
         # get meta data
         meta = get_metadata(data)
@@ -160,23 +144,7 @@ class Split(Filter):
                 data.set_metadata(meta)
 
         # find split according to schedule
-        split = None
-        for i in range(len(self.split_names)):
-            if (self._counter >= self._schedule[i]) and (self._counter < self._schedule[i + 1]):
-                split = self.split_names[i]
-
-        # set split
-        meta[META_SPLIT] = split
-
-        # update counter
-        self._counter += 1
-        if self._counter == self._schedule[-1]:
-            self._counter = 0
-
-        # update stats
-        if split not in self._stats:
-            self._stats[split] = 0
-        self._stats[split] += 1
+        meta[META_SPLIT] = self._splitter.next()
 
         return data
 
--- src/ldc/utils.py
+++ src/ldc/utils.py
@@ -1,4 +1,91 @@
-from typing import Dict, Optional
+import numpy as np
+
+from typing import Dict, Optional, List
+
+
+class Splitter:
+    """
+    Class for dividing a token stream into sub-streams.
+    """
+
+    def __init__(self, split_ratios: List[int], split_names: List[str]):
+        """
+        Initializes the splitter.
+
+        :param split_ratios: the list of (int) ratios to use (must sum up to 100)
+        :type split_ratios: list
+        :param split_names: the list of names to use for the splits
+        :type split_names: list
+        """
+        self.split_ratios = split_ratios
+        self.split_names = split_names
+        self._schedule = None
+        self._counter = 0
+        self._stats = None
+
+    def initialize(self):
+        """
+        Initializes the splitter, throws exceptions if undefined state or incorrect values.
+        """
+        if self.split_ratios is None:
+            raise Exception("No split ratios defined!")
+        if self.split_names is None:
+            raise Exception("No split name defined!")
+        if len(self.split_ratios) != len(self.split_names):
+            raise Exception("Differing number of split ratios and names: %d != %d" % (len(self.split_ratios), len(self.split_names)))
+        if sum(self.split_ratios) != 100:
+            raise Exception("Split ratios must sum up to 100, but got: %d" % sum(self.split_ratios))
+
+        # compute greatest common denominator and generate schedule
+        gcd = np.gcd.reduce(np.asarray(self.split_ratios))
+        self._schedule = [0] * (len(self.split_ratios) + 1)
+        for i, ratio in enumerate(self.split_ratios):
+            self._schedule[i + 1] = self._schedule[i] + ratio / gcd
+        self._counter = 0
+        self._stats = dict()
+
+    def reset(self):
+        """
+        Resets the counter and stats.
+        """
+        self._counter = 0
+        self._stats = dict()
+
+    def next(self) -> str:
+        """
+        Returns the next split name.
+
+        :return: the name of the split
+        :rtype: str
+        """
+        split = None
+        for i in range(len(self.split_names)):
+            if (self._counter >= self._schedule[i]) and (self._counter < self._schedule[i + 1]):
+                split = self.split_names[i]
+
+        # update counter
+        self._counter += 1
+        if self._counter == self._schedule[-1]:
+            self._counter = 0
+
+        # update stats
+        if split not in self._stats:
+            self._stats[split] = 0
+        self._stats[split] += 1
+
+        return split
+
+    def stats(self) -> Dict:
+        """
+        Returns the statistics.
+        """
+        return self._stats
+
+    def counter(self) -> int:
+        """
+        Returns the counter.
+        """
+        return self._counter
 
 
 def str_to_column_index(c: str, fail_on_nondigit=False):
