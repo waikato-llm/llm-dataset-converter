23a8edbe2cc16cca0ebab0102f7b2719336ecea2
Peter Reutemann <fracpete@gmail.com>
2023-10-31 16:54:33 +1300

    prepared for release

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         "seppl>=0.0.6",
         "pyyaml",
     ],
-    version="0.0.1",
+    version="0.0.2",
     author='Peter Reutemann',
     author_email='fracpete@waikato.ac.nz',
     entry_points={
