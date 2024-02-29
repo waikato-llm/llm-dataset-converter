66f12506673fa34f5990cee46e162ca37bcbc82d
Peter Reutemann <fracpete@gmail.com>
2024-02-29 13:29:32 +1300

    importing additional constants

--- src/ldc/filter/__init__.py
+++ src/ldc/filter/__init__.py
@@ -2,7 +2,7 @@ from ._assemble_sentences import AssembleSentences
 from ._change_case import ChangeCase, CASES, CASE_UNCHANGED, CASE_LOWER, CASE_UPPER, CASE_TITLE
 from ._file_filter import FileFilter
 from ._find_substr import FindSubstring
-from ._inspect import Inspect
+from ._inspect import Inspect, MODES, MODE_NONINTERACTIVE, MODE_INTERACTIVE, OUTPUTS, OUTPUT_STDOUT, OUTPUT_STDERR, OUTPUT_LOGGER, OUTPUT_FILE
 from ._keyword import Keyword
 from ._llama2_to_pairs import Llama2ToPairs
 from ._max_records import MaxRecords
