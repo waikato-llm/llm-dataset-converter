0a02b913290e961ffbe7ca5adb1c721dff582b12
Peter Reutemann <fracpete@gmail.com>
2024-05-06 13:36:12 +1200

    prepared for release

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         "pyyaml",
         "wai.logging",
     ],
-    version="0.2.2",
+    version="0.2.3",
     author='Peter Reutemann',
     author_email='fracpete@waikato.ac.nz',
     entry_points={
